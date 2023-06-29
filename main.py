088eab609dbfe1fb9532cc75e2662ef2f1fb63cd
Decode <85381286+decodethedev@users.noreply.github.com>
2023-06-29 10:24:36 +1000

    Unpatched bypass

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-import requests
 import re
+import tls_client
+import random
+import time
 
 linkvertise = "https://linkvertise.com/"
 
+session = tls_client.Session(
+    client_identifier="Firefox110",
+    random_tls_extension_order=True
+)
+
+browser_version = random.randint(110, 115)
+
 headers = {
-    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'accept-language': 'en-US,en;q=0.9',
+    # put your own headers here !
 }
 
 link = input("Enter Key Link: ")
 
 hwid = link.replace("https://flux.li/windows/start.php?HWID=","")
+
+if not link.startswith("https://flux.li/windows/start.php?HWID="):
+    print("Invalid link! It should look smth like this https://flux.li/windows/start.php?HWID=...")
+    exit()
 
 print(f"Got HWID: {hwid}")
 
@@ -30,7 +33,7 @@ endpoints =  [
         "referer": ""
     },
     {
-        "url": f"https://flux.li/windows/start.php?[OFFSET_REPLACE]=true&HWID={hwid}",
+        "url": f"https://flux.li/windows/start.php?[OFFSET_REPLACE]=false&HWID={hwid}",
         "referer": f"https://flux.li/windows/start.php?HWID={hwid}"
     },
     {
@@ -49,15 +52,25 @@ endpoints =  [
 
 OFFSET = ""
 
+safe_mode = False
+safe_mode_inp = input ("Do you want safe mode? This will delay the time between requests so it looks like you are just manually getting the key! (yes/no): \n >")
+
+if safe_mode_inp.lower() == "yes":
+    safe_mode = True
+    print("Safe mode enabled, this will delay the time between requests so it looks like you are just manually getting the key!")
+
 for i in range (len(endpoints)):
     url = endpoints[i]["url"].replace("[OFFSET_REPLACE]", OFFSET)
     referer = endpoints[i]["referer"]
 
     headers["referer"] = referer
-    response = requests.get(url, headers=headers)
+    response = session.get(
+        url,
+        headers=headers
+    )
     
     if i == 0:
-        match = re.search(r'start.php\?(.*?)=true&HWID=', response.text)
+        match = re.search(r'start.php\?(.*?)=false&HWID=', response.text)
         if match:
             OFFSET = match.group(1)
         else:
@@ -70,8 +83,8 @@ for i in range (len(endpoints)):
         with open("bypass.html", "w") as f:
             f.write(response.text)
         print(f"[{i}] Failed to bypass | Status code: {response.status_code}| Response content has been written to bypass.html for debugging purposes.")
-
-    print(f"[{i}] Response: {response.status_code} | {url}")
+ 
+    print(f"[{i}] Response: {response.status_code} | {url} | SAFEMODE: {safe_mode}")
 
     if i == len(endpoints)-1: # End of the bypass
         match = re.search(key_regex, response.text)
@@ -82,5 +95,8 @@ for i in range (len(endpoints)):
             with open("bypass.html", "w") as f:
                 f.write(response.text)
             print("Bypassed not successfully! Code: None, response content has been written to bypass.html for debugging purposes.")
-        
-        
+    
+    if safe_mode:
+        time.sleep(random.randint(4,7))
+
+input("Press enter to exit...")
